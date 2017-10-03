fd65ab86f6e0db0bd91fccece3b6ae330ce0a685
habbala <jesper.blomqvist@hotmail.com>
2017-10-03 11:16:10 +0200

    First push

---
+++ src/FaceImage.java
@@ -0,0 +1,7 @@
+public class FaceImage {
+
+
+    public FaceImage(){
+
+    }
+}
---
+++ src/Faces.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Faces {
+    //Read 300 files.
+    //Store as 2/3 as learning images and 1/3 as test images.
+    //Don't info that's not an image.
+    //Read 20x20 pixel file
+    //Pixel has 32 grey levels.
+
+    //training-file.txt
+    // training-facit.txt
+    // test-file.txt
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\temp\\testfile.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
