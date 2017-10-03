651f046a7fa07d6040b4cd79d778567240d77ee5
habbala <jesper.blomqvist@hotmail.com>
2017-10-03 12:30:19 +0200

    Fleshed out perceptron with constructor and summation

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,7 +1,17 @@
 public class NeuralNetwork {
     private Perceptron[] perceptrons;
 
-    public NeuralNetwork(int population) {
+    public NeuralNetwork(int population, int inputs) {
+
         this.perceptrons = new Perceptron[population];
+
+        for(int i = 0 ; i < population ; i++){
+            perceptrons[i] = new Perceptron(inputs,0);
+        }
     }
+
+    public void TrainNetwork(){
+
+    }
+
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,7 +1,23 @@
 public class Perceptron {
+    private double[] input, weigth;
+    private double bias;
+    private static ActivationFunction activationFunction;
 
-    public Perceptron(){
+    public Perceptron(int inputs, double bias){
+        activationFunction = new ActivationFunction();
+        this.input = new double[inputs];
+        this.weigth = new double[inputs];
+        this.bias = bias;
+    }
 
+    public double Summate(){
+        double sum = 0;
+
+        for(int i = 0 ; i < input.length ; i++){
+            sum += input[i] * weigth[i];
+        }
+
+        return bias + sum;
     }
 
 }
