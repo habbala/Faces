487be6ac968fc34f79f45cfb5fbc1876aede96c1
HeavyKladdkaka <aron.nisbel@hotmail.com>
2017-10-13 18:38:13 +0200

    Beginning to fix the whole neural network

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,8 +1,21 @@
 public class NeuralNetwork {
 
     private Perceptron[][] perceptrons;
+    private double moodArray;
+
+    private int ySize, xSize, inputs, sadCount, happyCount, mischievousCount,
+            angryCount, moodCount;
 
     public NeuralNetwork(int ySize, int xSize, int inputs) {
+
+        this.ySize = ySize;
+        this.xSize = xSize;
+        this.inputs = inputs;
+        this.sadCount = 0;
+        this.happyCount = 0;
+        this.mischievousCount = 0;
+        this.angryCount = 0;
+        this.moodCount = 0;
 
         this.perceptrons = new Perceptron[ySize][xSize];
 
@@ -11,6 +24,42 @@ public class NeuralNetwork {
                 perceptrons[y][x] = new Perceptron(inputs);
             }
         }
+    }
+
+    public FaceMood moodDetector(){
+
+        for(int y = 0 ; y < ySize ; y++) {
+            for (int x = 0; x < xSize; x++) {
+
+                if(perceptrons[y][x].isActivated()){
+
+                    switch(perceptrons[y][x].getFaceMood()) {
+
+                        case SAD:
+                            sadCount++;
+                            break;
+
+                        case HAPPY:
+                            happyCount++;
+                            break;
+
+                        case MISCHIEVOUS:
+                            mischievousCount++;
+                            break;
+
+                        case ANGRY:
+                            angryCount++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 4 ; i++){
+
+        }
+
+        return FaceMood.SAD;
     }
 
     public void trainNetwork(){
