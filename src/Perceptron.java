62759b513da4c13c31c51fc39c63ac8f2bf4563b
HeavyKladdkaka <aron.nisbel@hotmail.com>
2017-10-13 16:26:41 +0200

    Fixing perceptron

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,29 +1,50 @@
+import java.util.Random;
+
 public class Perceptron {
+
     private double[] input, weigth;
     private double bias, activationThreshold;
+    private Random randomValue;
+    private int firstRandomValue;
 
     public Perceptron(int inputs){
-        this.input = new double[inputs];
+
+        this.input = new double[inputs];//Byt ut mot inskickad input-array.
         this.weigth = new double[inputs];
         this.bias = 0;
-        activationThreshold = 0.5;
-    }
-
-    public int processInputs(){
-        for(int i = 0 ; i < input.length ; i++){
-            if (summateInput(i) >= 1){
-                return 1;
-            }
-        }
-        return 0;
+        this.activationThreshold = 0.5;
+        this.randomValue = new Random();
+        this.firstRandomValue = randomValue.nextInt(31);
     }
 
     int summateInput(int i){
-        if(input[i] * weigth[i] >
-                ActivationFunction.Sigmoid(activationThreshold)){
+
+        if(guessOutput() * weigth[i] > ActivationFunction.Sigmoid
+                (activationThreshold)){
+
             return 1;
+
         } else {
+
             return 0;
         }
+    }
+
+    public boolean isActivated(){
+
+        for(int i = 0 ; i < input.length ; i++){
+
+            if (summateInput(i) >= 1){
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public double guessOutput(){
+
+
     }
 }
